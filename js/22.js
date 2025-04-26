3421e5482eea99eb2cafb3798c51a768ca5fdf88
GribuzLord <gribuzcode@gmail.com>
2025-04-26 11:09:45 -0600

    Se estudio el optional chaining, el Nullish coalescing operator y la evaluacion del corto circuito

---
+++ js/22.js
@@ -0,0 +1,14 @@
+//Optional chaining '?'
+const alumno={
+    nombre:"Dylan",
+    clase: "Programacion 1",
+    aprobado:true
+
+}
+
+console.log(alumno.examenes?.examen1);
+
+console.log("Despues de alumno");
+
+//Nullish coalescing operator (??)
+
