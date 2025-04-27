cf010a3001974eaed13c66c63bd5759174d21513
GribuzLord <gribuzcode@gmail.com>
2025-04-27 11:12:17 -0600

    Se revisaron los modulos en JS

---
+++ js/24.js
@@ -0,0 +1,10 @@
+import {suma  as funcionSumar ,resta,multiplicar,dividir} from './funciones.js';
+
+const suma=funcionSumar(20,10);
+const restar=resta(20,10);
+const multi=multiplicar(20,10);
+const div=dividir(20,10);
+console.log(suma);
+console.log(restar);
+console.log(multi);
+console.log(div);
---
+++ js/funciones.js
@@ -0,0 +1,17 @@
+const suma=(n1,n2)=>n1+n2;
+
+const resta=(n1,n2)=>n1-n2;
+
+const multiplicar=(n1,n2)=>n1*n2;
+
+const dividir=(n1,n2)=>n1/n2;
+
+
+export{
+    suma,
+    resta,
+    multiplicar,
+    dividir
+}
+
+export default multiplicar;
