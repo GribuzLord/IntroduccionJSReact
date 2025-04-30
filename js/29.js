6db0a0d93cfcca0f2e2547dea110698be5ff1ca9
GribuzLord <gribuzcode@gmail.com>
2025-04-29 23:54:27 -0600

    Se repasó brevemente como interactuar, seleccionar y modificar elementos del DOM con JS

---
+++ js/29.js
@@ -0,0 +1,8 @@
+//Manipular elementos HTML con JS
+const heading=document.querySelector('.heading');
+const enlaces=document.querySelectorAll('.navegacion a');
+
+heading.textContent='Hola';
+
+const inputNombre=document.querySelector('#nombre');
+inputNombre.value="Un nuevo valor";
